4382cafbc959b40e6fc76a530c023dd54dc004ea
Ahmad <1ahmadkamal1@gmail.com>
2022-11-03 09:58:25 -0400

    last thing i did

--- mnist_example_pytorch.py
+++ mnist_example_pytorch.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 transform = torchvision.transforms.Compose([
                            torchvision.transforms.ToTensor(),
@@ -19,11 +23,18 @@ class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
         self.hidden = nn.Linear(784, 100)
+        self.batch_norm = torch.nn.BatchNorm1d(num_features=100)
+        self.more_hidden = [nn.Linear(100, 100) for _ in range(100)]
+        self.dropout_layer = torch.nn.Dropout(p=0.25)
         self.output = nn.Linear(100, 10)
 
     def forward(self, x):
         x = torch.flatten(x, 1)
         x = self.hidden(x)
+        # x = self.dropout_layer(x)
+        for layer in self.more_hidden:
+            x = layer(x)
+        x = self.batch_norm(x)
         x = F.relu(x)
         return self.output(x)
 
@@ -71,8 +82,8 @@ def test(model, test_loader):
 
 if __name__ == '__main__':
     model = Network()
-    optimizer = optim.Adam(model.parameters())
+    optimizer = optim.Adam(model.parameters(), weight_decay=2e-4)
 
-    for epoch in range(1, 5):
+    for epoch in range(1, 50):
         train(model, train_loader, optimizer, epoch)
         test(model, test_loader)
